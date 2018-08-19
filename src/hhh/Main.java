a6bce00870f3b09bb97f1ac074d585a49d303e18
SHAMIM <SHAMIM@SHAMIM-PC>
2018-08-19 17:37:48 +0530

    Inital Coomit hhhh

---
+++ src/hhh/Main.java
@@ -0,0 +1,22 @@
+package hhh;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SalesData data=new SalesData();
+data.display();
+displayGreeting();
+
+
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("Hello Happy Sales People");
+		System.out.println("This App Sales People");
+	}
+	
+	
+
+}
---
+++ src/hhh/SalesData.java
@@ -0,0 +1,11 @@
+package hhh;
+
+public class SalesData {
+	int data[]={0,1,2};
+	
+	public void display()
+	{
+		System.out.println("Implement Data Display");
+	}
+
+}
